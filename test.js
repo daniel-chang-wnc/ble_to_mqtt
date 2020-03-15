c9185f397c5f9ffc8b0b930a3f4b5c1c18b1266a
Daniel Chang <daniel.chang@wnc.com.tw>
2020-03-15 15:24:41 +0000

    Add Kalman filter

--- test.js
+++ test.js
@@ -1,17 +1,28 @@
 const BeaconScanner = require('node-beacon-scanner');
 const scanner = new BeaconScanner();
 const mqtt = require('mqtt')
+const KalmanFilter = require('kalmanjs');
 const os = require('os')
-const client  = mqtt.connect('mqtt://192.168.1.110')
+const client  = mqtt.connect('mqtt://raspberrypi1.local')
 
+const kf = new KalmanFilter();
 
 
 // Set an Event handler for becons
 scanner.onadvertisement = (ad) => {
   console.log(os.hostname())
-  console.log(JSON.stringify(ad, null, '  '));
+  //console.log(JSON.stringify(ad, null, '  '));
   console.log(ad.id)
-  client.publish(`beacon/${ad.id}`,JSON.stringify(ad, null, '  '))
+  console.log(ad.rssi)
+  console.log(kf.filter(ad.rssi))
+  obj_pub ={
+    host: os.hostname(),
+    id: ad.id, 
+    origin_rssi: ad.rssi, 
+    kf_rssi: kf.filter(ad.rssi)
+  }
+
+  client.publish(`beacon/${ad.id}`,JSON.stringify(obj_pub, null, '  '))
 
 };
 
