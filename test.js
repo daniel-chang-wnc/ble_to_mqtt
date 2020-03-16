25382ed2fba80ca16bf234470e64782b5f7924e2
Your Name <you@example.com>
2020-03-16 12:01:22 +0800

    add floor config

--- test.js
+++ test.js
@@ -4,9 +4,14 @@ const mqtt = require('mqtt')
 const KalmanFilter = require('kalmanjs');
 const os = require('os')
 const client  = mqtt.connect('mqtt://raspberrypi1.local')
-
 const kf = new KalmanFilter();
-
+const fs = require('fs');
+var config
+fs.readFile('config.json', (err, data) => {
+	    if (err) throw err;
+	    config = JSON.parse(data);
+	    console.log(config.floor);
+});
 
 // Set an Event handler for becons
 scanner.onadvertisement = (ad) => {
@@ -17,6 +22,7 @@ scanner.onadvertisement = (ad) => {
   console.log(kf.filter(ad.rssi))
   obj_pub ={
     host: os.hostname(),
+    floor:config.floor,
     id: ad.id, 
     origin_rssi: ad.rssi, 
     kf_rssi: kf.filter(ad.rssi)
